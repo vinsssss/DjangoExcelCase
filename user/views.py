308373d9839282fbccee691569f24471748d57aa
vins <syi@mail.ustc.edu.cn>
2020-11-26 21:59:57 +0800

    完成登录、注册部分

--- user/views.py
+++ user/views.py
@@ -4,48 +4,86 @@ from django.contrib.auth.models import User
 from django.views import View
 
 from user import models
+from user.form import UserForm, RegisterForm
 
 
 class LoginView(View):
 
     def get(self, request):
-        return render(request, 'login.html')
+        if request.session.get('is_login', None):
+            # print(request.session['is_login'])
+            return redirect('/index')
+        else:
+            login_form = UserForm()
+            return render(request, 'login.html', locals())
 
     def post(self, request):
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
-        message = "所有字段都必须填写！"
-        if username and password:  # 确保用户名和密码都不为空
-            username = username.strip()
-            # 用户名字符合法性验证
-            # 密码长度验证
-            # 更多的其它验证.....
+        if request.session.get('is_login', None):
+            return redirect('/index')
+        login_form = UserForm(request.POST)
+        message = "请检查填写的内容！"
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(username=username)
                 if user.password == password:
+                    # 是否登录标志
+                    request.session['is_login'] = True
+                    request.session['user_id'] = user.id
+                    request.session['user_name'] = user.username
                     return redirect('/index/')
                 else:
                     message = "密码不正确！"
             except:
-                message = "用户名不存在！"
-        return render(request, 'login.html', {"message": message})
-
-
-class RegisterView(View):
-
-    def get(self, request):
-        if request.method == "GET":
-            return render(request, "register.html")
-
-    def post(self, request):
-        if request.method == "POST":
-            return render(request, "login.html")
+                message = "用户不存在！"
+        return render(request, 'login.html', locals())
 
 
 class LogoutView(View):
 
     def get(self, request):
-        if request.method == "GET":
-            return render(request, "index.html")
+        if not request.session.get('is_login', None):
+            # 如果本来就未登录，也就没有登出一说
+            return redirect("/login")
+        else:
+            request.session.flush()
+            return redirect("/login")
 
-# Create your views here.
+
+class RegisterView(View):
+
+    def get(self, request):
+        register_form = RegisterForm()
+        return render(request, 'register.html', locals())
+
+    def post(self, request):
+        register_form = RegisterForm(request.POST)
+        message = "[ERROR]请检查填写的内容！"
+        if register_form.is_valid():  # 获取数据
+            username = register_form.cleaned_data['username']
+            password1 = register_form.cleaned_data['password1']
+            password2 = register_form.cleaned_data['password2']
+            email = register_form.cleaned_data['email']
+            if password1 != password2:  # 判断两次密码是否相同
+                message = "两次输入的密码不同！"
+                return render(request, 'register.html', locals())
+            else:
+                same_name_user = models.User.objects.filter(username=username)
+                if same_name_user:  # 用户名唯一
+                    message = '用户已经存在，请重新选择用户名！'
+                    return render(request, 'register.html', locals())
+                same_email_user = models.User.objects.filter(email=email)
+                if same_email_user:  # 邮箱地址唯一
+                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                    return render(request, 'register.html', locals())
+
+                # 当一切都OK的情况下，创建新用户
+                new_user = models.User.objects.create()
+                new_user.username = username
+                new_user.password = password1
+                new_user.email = email
+                new_user.save()
+                return redirect('/login/')  # 自动跳转到登录页面
+        else:
+            return render(request, 'register.html', locals())
