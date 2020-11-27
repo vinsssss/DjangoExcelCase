6c3e8f1a07aa4b438752b76399132b667ec088df
vins <syi@mail.ustc.edu.cn>
2020-11-27 11:10:27 +0800

    完善登录、注册部分

--- display/views.py
+++ display/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.views import View
@@ -7,4 +7,10 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request):
-        return render(request, "index.html")
+        if request.session.get('is_login', None):
+            return render(request, 'index.html', {'is_login': request.session['is_login'],
+                                                  'user_name': request.session['user_name']})
+        else:
+            # message = '请登录！'
+            return redirect('/login/')
+            # return render(request, "login.html")
--- user/views.py
+++ user/views.py
@@ -6,20 +6,33 @@ from django.views import View
 from user import models
 from user.form import UserForm, RegisterForm
 
+import hashlib
+
+
+def hash_code(s, salt='mysite'):
+    h = hashlib.sha256()
+    s += salt
+    # update方法只接收bytes类型
+    h.update(s.encode())
+    return h.hexdigest()
+
 
 class LoginView(View):
 
     def get(self, request):
         if request.session.get('is_login', None):
+            # print("###################")
+            # print(request.session['user_name'])
             # print(request.session['is_login'])
-            return redirect('/index')
+            return redirect('/index/')
         else:
             login_form = UserForm()
+            # print(locals())
             return render(request, 'login.html', locals())
 
     def post(self, request):
         if request.session.get('is_login', None):
-            return redirect('/index')
+            return redirect('/index/')
         login_form = UserForm(request.POST)
         message = "请检查填写的内容！"
         if login_form.is_valid():
@@ -27,7 +40,7 @@ class LoginView(View):
             password = login_form.cleaned_data['password']
             try:
                 user = models.User.objects.get(username=username)
-                if user.password == password:
+                if user.password == hash_code(password):
                     # 是否登录标志
                     request.session['is_login'] = True
                     request.session['user_id'] = user.id
@@ -81,7 +94,7 @@ class RegisterView(View):
                 # 当一切都OK的情况下，创建新用户
                 new_user = models.User.objects.create()
                 new_user.username = username
-                new_user.password = password1
+                new_user.password = hash_code(password1)
                 new_user.email = email
                 new_user.save()
                 return redirect('/login/')  # 自动跳转到登录页面
